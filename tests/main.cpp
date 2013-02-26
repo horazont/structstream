f906ad13285293584bd63edd2f67e7a8f9e11820
Jonas Wielicki <j.wielicki@sotecware.net>
2013-02-26 12:10:33 +0100

    Use colours in Catch test output

--- tests/main.cpp
+++ tests/main.cpp
@@ -24,4 +24,5 @@ For feedback and questions about structstream++ please e-mail one of the
 authors named in the AUTHORS file.
 **********************************************************************/
 #define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_USE_ANSI_COLOUR_CODES
 #include <catch.hpp>
