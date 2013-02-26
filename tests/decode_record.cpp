d0316b27e18f1a0a82f68e387fa8a615eaef89fd
Jonas Wielicki <j.wielicki@sotecware.net>
2013-02-26 13:50:00 +0100

    Make record tests use compliant structstream blobs

--- tests/decode_record.cpp
+++ tests/decode_record.cpp
@@ -34,7 +34,8 @@ using namespace StructStream;
 TEST_CASE ("decode/records/uint32", "Test decode of a uint32 record")
 {
     static const uint8_t data[] = {
-        (uint8_t)(RT_UINT32) | 0x80, uint8_t(0x01) | 0x80, 0x12, 0x34, 0x56, 0x78
+        (uint8_t)(RT_UINT32) | 0x80, uint8_t(0x01) | 0x80, 0x12, 0x34, 0x56, 0x78,
+        (uint8_t)(RT_END_OF_CHILDREN) | 0x80
     };
 
     RegistryHandle registry = RegistryHandle(new Registry());
@@ -53,7 +54,8 @@ TEST_CASE ("decode/records/uint64", "Test decode of a uint64 record")
 {
     static const uint8_t data[] = {
         (uint8_t)(RT_UINT64) | 0x80, uint8_t(0x01) | 0x80,
-            0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xFF
+            0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xFF,
+        (uint8_t)(RT_END_OF_CHILDREN) | 0x80
     };
 
     RegistryHandle registry = RegistryHandle(new Registry());
@@ -71,7 +73,8 @@ TEST_CASE ("decode/records/uint64", "Test decode of a uint64 record")
 TEST_CASE ("decode/records/int32", "Test decode of a int32 record")
 {
     static const uint8_t data[] = {
-        (uint8_t)(RT_INT32) | 0x80, uint8_t(0x01) | 0x80, 0x12, 0x34, 0x56, 0x78
+        (uint8_t)(RT_INT32) | 0x80, uint8_t(0x01) | 0x80, 0x12, 0x34, 0x56, 0x78,
+        (uint8_t)(RT_END_OF_CHILDREN) | 0x80
     };
 
     RegistryHandle registry = RegistryHandle(new Registry());
@@ -90,7 +93,8 @@ TEST_CASE ("decode/records/int64", "Test decode of a int64 record")
 {
     static const uint8_t data[] = {
         (uint8_t)(RT_INT64) | 0x80, uint8_t(0x01) | 0x80,
-            0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xFF
+            0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xFF,
+        (uint8_t)(RT_END_OF_CHILDREN) | 0x80
     };
 
     RegistryHandle registry = RegistryHandle(new Registry());
@@ -111,7 +115,8 @@ TEST_CASE ("decode/records/blob", "Test decode of a blob record")
         (uint8_t)(RT_BLOB) | 0x80, uint8_t(0x01) | 0x80,
         // length
         uint8_t(0x0D) | 0x80,
-        0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x00
+        0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x00,
+        (uint8_t)(RT_END_OF_CHILDREN) | 0x80
     };
     static const char reference[] = "Hello World!";
 
@@ -140,7 +145,8 @@ TEST_CASE ("decode/records/utf8", "Test decode of a utf8 record")
         (uint8_t)(RT_UTF8STRING) | 0x80, uint8_t(0x01) | 0x80,
         // length
         uint8_t(0x0C) | 0x80,
-        0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21
+        0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21,
+        (uint8_t)(RT_END_OF_CHILDREN) | 0x80
     };
     static const char reference[] = "Hello World!";
 
